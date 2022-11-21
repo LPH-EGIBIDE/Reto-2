9f358cd740e28bec389ed3039832eba00edd0cb1
DRomeroL-ES <damian.romero@ikasle.egibide.org>
2022-11-21 12:49:35 +0100

    manageUser added

--- Controllers/api/users/manageUser.php
+++ Controllers/api/users/manageUser.php
@@ -1,1 +1,94 @@
 <?php
+
+use Entities\UserEntity;
+use Repositories\UserRepository;
+use Utils\AuthUtils;
+use Utils\TOTP;
+
+require_once '../../../config.inc.php';
+session_start();
+
+if (!AuthUtils::checkAuth())
+    die(json_encode(["status" => "error", "message" => "No hay sesión iniciada"]));
+
+$user = $_SESSION["user"];
+
+function activateMFA(UserEntity $user) {
+    $user->setMfaData(TOTP::generatePrivateKey());
+    $user->setMfaType(1);
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "MFA por aplicación activado correctamente"]);
+}
+
+function activateEmailMFA(UserEntity $user) {
+    $user->setMfaType(2);
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "MFA por correo activado correctamente"]);
+}
+
+function deactivateMFA(UserEntity $user) {
+    $user->setMfaData(null);
+    $user->setMfaType(0);
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "MFA desactivado correctamente"]);
+}
+
+function changePassword(UserEntity $user, string $oldPassword, string $newPassword) {
+    if (!password_verify($oldPassword, $user->getPassword()))
+        die(json_encode(["status" => "error", "message" => "La contraseña actual no es correcta"]));
+    $user->setPassword(UserEntity::hashPassword($newPassword));
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "Contraseña cambiada correctamente"]);
+}
+
+function changeEmail(UserEntity $user, string $newEmail) {
+    $user->setEmail($newEmail);
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "Correo cambiado correctamente"]);
+}
+
+function changeUsername(UserEntity $user, string $newUsername) {
+    $user->setUsername($newUsername);
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "Nombre de usuario cambiado correctamente"]);
+}
+
+function changeAvatar(UserEntity $user, string $newAvatar) {
+    $user->setAvatar($newAvatar);
+    UserRepository::updateUser($user);
+    $_SESSION["user"] = $user;
+    echo json_encode(["status" => "success", "message" => "Foto de perfil cambiada correctamente"]);
+}
+
+switch ($_POST["method"]) {
+    case "activateMFA":
+        activateMFA($user);
+        break;
+    case "activateEmailMFA":
+        activateEmailMFA($user);
+        break;
+    case "deactivateMFA":
+        deactivateMFA($user);
+        break;
+    case "changePassword":
+        changePassword($user, $_POST["oldPassword"], $_POST["newPassword"]);
+        break;
+    case "changeEmail":
+        changeEmail($user, $_POST["newEmail"]);
+        break;
+    case "changeUsername":
+        changeUsername($user, $_POST["newUsername"]);
+        break;
+    case "changeAvatar":
+        changeAvatar($user, $_POST["newAvatar"]);
+        break;
+    default:
+        echo json_encode(["status" => "error", "message" => "Método no soportado"]);
+        break;
+}
