d46291e8d69a86b21f0424eaa5b59b23a680b247
Alex <alexcortes90@gmail.com>
2022-11-22 02:32:30 +0100

    Arreglar el css de m..

--- Views/Navigation/header.php
+++ Views/Navigation/header.php
@@ -8,20 +8,22 @@ if (!AuthUtils::checkAuth()) {
 }
 
 $user = $_SESSION['user'];
-
+$title = empty($title) ? "WTFAQ" : $title;
 ?>
 <!DOCTYPE html>
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/assets/stylesheets/paginaPrin.css">
+    <title><?= $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 
 <div class="container" id="container">
 
 <header class="cabecera">
-    <img src="/assets/img/lph-black.png" alt="logo.png">
-    <h4>El saber es poder</h4>
+    <img src="/assets/img/lph-black.png" class="altLogo" alt="Logo cabecera">
 </header>
 
 <nav class="navegador">
@@ -29,28 +31,36 @@ $user = $_SESSION['user'];
         <li><a href="#">Preguntas</a></li>
         <li><a href="#">Respuestas</a></li>
     </ul>
-    <img src="/assets/img/lph-logo.png" alt="logo.png"> 
+    <div class="logoMobile">
+        <img src="/assets/img/lph-logo.png" class="altLogo"  alt="logo.png">
+    </div>
+
     <ul class="navD">
         
-        <li class="usuario"><?= $user->getUsername() ?>
+        <li class="usuario">
+            <div class="userProfile">
+                <img src="/api/attachments/id/<?= $user->getAvatar()->getId() ?>" id="userLogo" alt="">
+                <span id="username"><?= $user->getUsername() ?></span>
+            </div>
                 <ul class="opUsuario">
                     <li><a href="/user/profile">Mi perfil</a></li>
                     <li><a href="#">Pregunta favorita</a></li>
                     <li><a href="/logout">Cerrar sesion</a></li>
                 </ul>
         </li>
-        <li class="notificaciones">Notificaciones
-            <div class="listaNoti">
-                <div class="contenidoNoti">
-                  <h5>Alertas</h5>
+        <li class="notificaciones">
+            <div id="notifications"><i class="fa-solid fa-bell"></i> <span id="notifiText">Notificaciones</span></div>
+            <div class="listaNoti hoverElement">
+                <div class="contenidoNoti hoverElement">
+                  <h3>Alertas</h3>
                   <hr>
-                  <div class="notificacion">
-                    <i class="fa-regular fa-bell" id="notifiIcon"></i>
+                  <div class="notificacion hoverElement">
+                    <i class="fa-regular fa-bell notifiIcon" ></i>
                     <p class="mensaje">Mensaje</p>
                     <div class="fecha">
-                     <p>Hace: <p class="tiempo"> 1 dia</p></p>
+                        <p>Hace: 1 dia</p>
                     </div>
-                    <i class="fa-solid fa-trash" id="basura"></i>
+                    <i class="fa-solid fa-trash basura" ></i>
                   </div>
 
                 </div>
