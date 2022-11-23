04d77f8bce749a72738c9606322b57b8a0702848
ImAleeexx <52298617+ImAleeexx@users.noreply.github.com>
2022-11-23 09:11:34 +0100

    Fix conflict

--- Views/post.php
+++ Views/post.php
@@ -1,103 +1,51 @@
-<<<<<<< HEAD
 <div class="main">
     <div class="relleno">
-        <div id="cajaPregunta">
+        <div class="cajaPregunta" id="cajaPreguntaSkeleton">
             <div class="conTags">
-                <p class="tags">Tags</p>
+                <div class="c-skeleton__text"></div>
             </div>
             <div class="conPreguntaTitu">
-                <h3 class="preguntaTitu">Titulo pregunta</h3>
+                <div class="c-skeleton__text c-skeleton__text--full c-skeleton__text--small-height"></div>
             </div>
             <div class="conTextoPre">
-                
-                    <p class="textoPre">Pregunta</p>
-                
+
+                <div class="c-skeleton__text"></div>
+
             </div>
             <div class="conAdjunto">
-                <p class="adjunto">Adjunto</p>
+                <div class="c-skeleton__text"></div>
             </div>
             <div class="conArchivos">
-                <p class="archivos">Archivos</p>
+                <div class="c-skeleton__text"></div>
             </div>
-    </div>
+        </div>
 
     <div id="medio">
         <h3 id="tituRes">Respuestas:</h3><input type="button" value="Añadir" width="2em" height="2em" id="anadir">
     </div>
 
-   
-    <div id="cajaRespuesta">
-            <div class="conTextoRes">
-                <p class="textoRes">Respuesta</p>
-            </div>
-            <div class="conAdjunto2">
-                <p class="adjunto">Adjunto</p>
-            </div>
-            <div class="conArchivosRe">
-                <p class="archivosRe">Archivos</p>
-            </div>
-            <div class="contenedorAbajo">
-             <div class="conUsuario">
-                <p>Por: <a class="textoUsuario">Usuario</a></p>
-             </div>
-             <div class="parteDrc">
-                <i class="fa-solid fa-star" id="star"></i>
-                <i class="fa-solid fa-up" id="up"></i>
-             </div>
-            </div>
-    </div>
+   <div id="contenedorRespuestas">
+       <div class="cajaRespuesta">
+           <div class="conTextoRes">
+               <div class="c-skeleton__text skeletonRes"></div>
+           </div>
+           <div class="conAdjunto2">
+               <div class="c-skeleton__text"></div>
+           </div>
+           <div class="conArchivosRe">
+               <div class="c-skeleton__text c-skeleton__text--full c-skeleton__text--small-height"></div>
+           </div>
+           <div class="contenedorAbajo">
+               <div class="conUsuario">
+                   <div class="c-skeleton__text c-skeleton__text--small"></div>
+               </div>
+               <div class="parteDrc">
+                   <span class="star"><i class="fa-solid fa-star"></i></span>
+                   <span class="up"><i class="fa-solid fa-up"></i></span>
+               </div>
+           </div>
+       </div>
+   </div>
 
     </div>
-</div>	
-=======
-<container class="main">
-    <div class="relleno">
-        <div id="cajaPregunta">
-            <div class="conTags">
-                <p class="tags">Tags</p>
-            </div>
-            <div class="conPreguntaTitu">
-                <h3 class="preguntaTitu">Titulo pregunta</h3>
-            </div>
-            <div class="conTextoPre">
-                
-                    <p class="textoPre">Pregunta</p>
-                
-            </div>
-            <div class="conAdjunto">
-                <p class="adjunto">Adjunto</p>
-            </div>
-            <div class="conArchivos">
-                <p class="archivos">Archivos</p>
-            </div>
-    </div>
-
-    <div id="medio">
-        <h3 id="tituRes">Respuestas:</h3><input type="button" value="Añadir" width="2em" height="2em" id="anadir">
-    </div>
-
-   
-    <div id="cajaRespuesta">
-            <div class="conTextoRes">
-                <p class="textoRes">Respuesta</p>
-            </div>
-            <div class="conAdjunto2">
-                <p class="adjunto">Adjunto</p>
-            </div>
-            <div class="conArchivosRe">
-                <p class="archivosRe">Archivos</p>
-            </div>
-            <div class="contenedorAbajo">
-             <div class="conUsuario">
-                <p>Por: <a class="textoUsuario">Usuario</a></p>
-             </div>
-             <div class="parteDrc">
-                <i class="fa-solid fa-star" id="star"></i>
-                <i class="fa-solid fa-up" id="up"></i>
-             </div>
-            </div>
-    </div>
-
-    </div>
-</container>	
->>>>>>> d46291e8d69a86b21f0424eaa5b59b23a680b247
+</div>
