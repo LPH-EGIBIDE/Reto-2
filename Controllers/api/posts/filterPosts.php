8a104958e48e2e0b6c913695aac3c9b42e077781
Alex <alexcortes90@gmail.com>
2022-11-25 03:36:08 +0100

    Filter posts fixed and author removes

--- Controllers/api/posts/filterPosts.php
+++ Controllers/api/posts/filterPosts.php
@@ -12,20 +12,21 @@ header('Content-Type: application/json');
 if (!AuthUtils::checkAuth())
     die(json_encode(["status" => "error", "message" => "No hay sesión iniciada"]));
 
-$offset = intval($_GET['offset']) ?? intval($_POST['offset']) ?? 20;
-$startFrom = intval($_GET['startFrom']) ?? intval($_POST['startFrom']) ??  1;
+$offset = intval($_GET['offset'] ?? $_POST['offset'] ?? 20);
+$startFrom = intval($_GET['startFrom'] ?? $_POST['startFrom'] ??  0);
 $title = $_GET['title'] ?? $_POST['title'] ?? "";
-$topic = intval($_GET['topic']) ?? intval($_POST['topic']) ?? null;
-$user = intval($_GET['user']) ?? intval($_POST['user']) ?? null;
+$topic = intval($_GET['topic'] ?? $_POST['topic'] ?? -1) == -1 ? null : intval($_GET['topic'] ?? $_POST['topic'] ?? -1);
 $sort = $_GET['sort'] ?? "DATE";
 $sortOrder = $_GET['sortOrder'] ?? "DESC";
 
 $data =[];
-$data['pages'] = ceil(PostRepository::getPostsCount() / $offset);
 try {
     $posts = PostRepository::filterPosts($offset, $startFrom, $title, $topic, $sort, $sortOrder);
+    $data['posts'] = $posts;
+    $data['postCount'] = count($posts);
+
 } catch (DataNotFoundException $e) {
     die(json_encode(["status" => "error", "message" => $e->getMessage()]));
 }
 
-echo json_encode($posts);
+echo json_encode($data);
