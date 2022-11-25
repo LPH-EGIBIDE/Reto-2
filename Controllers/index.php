9e7d2c9ded684981934cebf330459e0b4d4534fc
Alex <alex@imaleex.com>
2022-11-25 08:29:55 +0100

    Rename files

--- Controllers/index.php
+++ Controllers/index.php
@@ -1,15 +1,18 @@
 <?php
 require_once __DIR__.'/../config.inc.php';
-session_start();
 
 use Utils\AuthUtils;
+
+session_start();
+$importsCss = ["/assets/stylesheets/searchPosts.css"];
 
 if (!AuthUtils::checkAuth()) {
     header("Location: /login");
     exit();
 }
+
 $user = $_SESSION['user'];
 
-
 require APP_ROOT.'Views/Navigation/header.php';
+require APP_ROOT.'Views/searchPosts.php';
 require APP_ROOT.'Views/Navigation/footer.php';
